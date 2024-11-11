55e2e203955e473eec04e8c9b8dde66162b8a77d
Matt Iakhno <matt.iakhno@gmail.com>
2024-11-10 19:41:20 -0800

    Toggle play using context instead of audio ref method

--- src/lib/useMediaSessionHandlers.ts
+++ src/lib/useMediaSessionHandlers.ts
@@ -1,35 +1,24 @@
 import { useEffect, RefObject } from "react";
+import { usePlayer } from "@/contexts";
 
 export const useMediaSessionHandlers = (audioRef: RefObject<HTMLAudioElement>) => {
+  const {
+    isPlaying,
+    togglePlay,
+  } = usePlayer();
   useEffect(() => {
     if ("mediaSession" in navigator) {
       // Set the play action handler
       navigator.mediaSession.setActionHandler("play", () => {
-        if (audioRef.current) {
-          audioRef.current.play();
+        if (audioRef.current && !isPlaying) {
+          togglePlay();
         }
       });
 
       // Set the pause action handler
       navigator.mediaSession.setActionHandler("pause", () => {
-        if (audioRef.current) {
-          audioRef.current.pause();
-        }
-      });
-
-      // Optional seekbackward handler with type checking
-      navigator.mediaSession.setActionHandler("seekbackward", (details: MediaSessionActionDetails) => {
-        if (audioRef.current) {
-          const skipTime = details.seekOffset || 10; // default to 10 seconds if seekOffset is not provided
-          audioRef.current.currentTime = Math.max(audioRef.current.currentTime - skipTime, 0);
-        }
-      });
-
-      // Optional seekforward handler with type checking
-      navigator.mediaSession.setActionHandler("seekforward", (details: MediaSessionActionDetails) => {
-        if (audioRef.current) {
-          const skipTime = details.seekOffset || 10;
-          audioRef.current.currentTime = Math.min(audioRef.current.currentTime + skipTime, audioRef.current.duration);
+        if (audioRef.current && isPlaying) {
+          togglePlay();
         }
       });
     }
@@ -39,9 +28,7 @@ export const useMediaSessionHandlers = (audioRef: RefObject<HTMLAudioElement>) =
       if ("mediaSession" in navigator) {
         navigator.mediaSession.setActionHandler("play", null);
         navigator.mediaSession.setActionHandler("pause", null);
-        navigator.mediaSession.setActionHandler("seekbackward", null);
-        navigator.mediaSession.setActionHandler("seekforward", null);
       }
     };
-  }, [audioRef]);
+  }, [audioRef, isPlaying, togglePlay]);
 };
