a3ffac166d4cda3b09139e909f24489b1f4c82c2
Matt Iakhno <matt.iakhno@gmail.com>
2025-11-03 20:19:35 -0800

    Service worker to enable better background memory management

---
+++ src/service-worker.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = "collective-radio-cache-v1";
+const urlsToCache = ["/", "/index.html", "/manifest.webmanifest"];
+
+// Install event: Cache app shell
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Fetch event: Serve cached content when offline
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// Activate event: Clean up old caches
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((cacheName) => cacheName !== CACHE_NAME)
+          .map((cacheName) => caches.delete(cacheName))
+      );
+    })
+  );
+});
