c3090e259253de7f3a2ffbe114831d0e4611343f
Matt Iakhno <matt.iakhno@gmail.com>
2025-11-06 21:22:56 -0800

    Recalculate app height on warm launches

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import Hero from "./components/Hero";
 import Media from "./components/Media";
 
@@ -11,6 +12,34 @@ import "./App.css";
 import { AppProvider } from "@/contexts/AppContext";
 
 function App() {
+  // when the app is launched, calculate the app height
+  // there is a known limitation with PWA where this doesn't get set
+  // correctly on warm launches
+  useEffect(() => {
+    const setAppHeight = () => {
+      document.documentElement.style.setProperty(
+        "--app-height",
+        `${window.innerHeight}px`
+      );
+    };
+
+    const handleVisibilityChange = () => {
+      if (document.visibilityState === "visible") setAppHeight();
+    };
+
+    setAppHeight();
+
+    window.addEventListener("resize", setAppHeight);
+    window.addEventListener("orientationchange", setAppHeight);
+    document.addEventListener("visibilitychange", handleVisibilityChange);
+
+    return () => {
+      window.removeEventListener("resize", setAppHeight);
+      window.removeEventListener("orientationchange", setAppHeight);
+      document.removeEventListener("visibilitychange", handleVisibilityChange);
+    };
+  }, []);
+
   return (
     <AppProvider>
       <Hero />
