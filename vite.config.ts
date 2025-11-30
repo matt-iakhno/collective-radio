32b4e620eed1694dbec8c2628c18c67b5b267b9b
Matt Iakhno <matt.iakhno@gmail.com>
2025-11-30 14:59:40 -0800

    Service worker hash fix, android favicon size fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,17 +3,22 @@ import react from '@vitejs/plugin-react'
 import path from "path"
 import { viteStaticCopy } from "vite-plugin-static-copy";
 
+const BUILD_TIME_VALUE = JSON.stringify(Date.now());
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: "src/service-worker.js",
-          dest: ""
+  plugins: [react(),viteStaticCopy({
+    targets: [
+      {
+        src: "src/service-worker.js",
+        dest: "",
+        // 👇 Use a transform function to replace the placeholder
+        transform: (content) => {
+          return content.toString().replace(/__BUILD_TIME__/g, BUILD_TIME_VALUE);
         }
-      ]
-    })
+      }
+    ]
+  })
   ],
   resolve: {
     alias: {
@@ -27,8 +32,5 @@ export default defineConfig({
         main: path.resolve(__dirname, "index.html"),
       },
     }
-  },
-  define: {
-    __BUILD_TIME__: JSON.stringify(Date.now()), // inject timestamp
   },
 })
