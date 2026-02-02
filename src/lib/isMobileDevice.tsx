cec56d39d9381686bb56d4ab9d0eef4ae39511a8
Matt Iakhno <matt.iakhno@gmail.com>
2026-02-01 20:32:37 -0800

    Guard navigator usage

--- src/lib/isMobileDevice.tsx
+++ src/lib/isMobileDevice.tsx
@@ -1,3 +1,6 @@
 export const isMobileDevice = (): boolean => {
+  if (typeof navigator === "undefined") {
+    return false;
+  }
   return /Mobi|Android|iPhone|iPad|iPod/i.test(navigator.userAgent);
 };
